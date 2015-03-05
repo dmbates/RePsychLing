4001ade387797a87b686b67dea6a2e5cb9b7b3bc
Douglas Bates <dmbates@gmail.com>
2015-03-05 17:11:45 +0100

    Update package contents, especially vignettes

--- R/rePCA.R
+++ R/rePCA.R
@@ -1,5 +1,16 @@
+##' PCA of random-effects variance-covariance estimates
+##'
+##' Perform a Principal Components Analysis (PCA) of the random-effects
+##' variance-covariance estimates from a fitted mixed-effects model
+##' @title PCA of random-effects
+##' @param x a merMod object
+##' @return a \code{prcomplist} object
+##' @author Douglas Bates
+##' @export
+rePCA <- function(x) UseMethod('rePCA')
+
 #' @export
-prcomp.merMod <- function(x) {
+rePCA.merMod <- function(x) {
     chfs <- getME(x,"Tlist")  # list of lower Cholesky factors
     nms <- names(chfs)
     unms <- unique(nms)
